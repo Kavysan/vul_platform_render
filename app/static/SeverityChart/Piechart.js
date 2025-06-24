541fb5873cf04510c6271db2c0debd833db64970
Kavya Santhal Kumar <kavyak@arraynetworks.net>
2025-06-24 11:22:48 -0700

    pie chart change

--- app/static/SeverityChart/Piechart.js
+++ app/static/SeverityChart/Piechart.js
@@ -6,7 +6,9 @@ document.addEventListener('DOMContentLoaded', function () {
     // if (typeof productVulnerabilityData !== 'undefined') {
         const ctx = document.getElementById('productVulnerabilityPieChart').getContext('2d');
 
-        const labels = Object.keys(productVulnerabilityData);
+        const rawLabels = Object.keys(productVulnerabilityData);
+        const labels = rawLabels.map(label => label.split(' ')[0]);  
+
         const data = Object.values(productVulnerabilityData);
 
         new Chart(ctx, {
@@ -17,11 +19,11 @@ document.addEventListener('DOMContentLoaded', function () {
                     label: 'Number of Vulnerabilities',
                     data: data,
                     backgroundColor: [
-                        'rgba(255, 99, 132, 0.7)',
-                        'rgba(255, 159, 64, 0.7)',
-                        'rgba(255, 205, 86, 0.7)',
-                        'rgba(75, 192, 192, 0.7)',
-                        'rgba(54, 162, 235, 0.7)'
+                        'rgba(153, 102, 255, 0.7)',  // purple
+                        'rgba(255, 99, 71, 0.7)',    // tomato red
+                        'rgba(81, 210, 139, 0.7)',   // medium sea green
+                        'rgba(243, 210, 64, 0.7)',    // dark orange
+                        'rgba(100, 149, 237, 0.7)'   // cornflower blue
                     ],
                     borderWidth: 1
                 }]
